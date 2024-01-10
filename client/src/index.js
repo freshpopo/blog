7b62a4c32184aeabcb8b583c328191cb29435ba8
popo <freshposong@gmail.com>
2024-01-10 11:24:02 +0900

    redux 세팅

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
-import App from './App';
+import { Provider } from 'react-redux';
+import {applyMiddleware, createStore} from "redux";
+import promiseMiddleware from 'redux-promise';
+import {thunk} from 'redux-thunk';
+
+import App from 'App';
+import Reducer from '_reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, thunk)(createStore);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Provider store={createStoreWithMiddleware(Reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App/>
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
