cddb03946f7f9660375e49349353bf16905602ec
popo <freshposong@gmail.com>
2024-01-12 17:23:08 +0900

    로그아웃 api 연결

--- client/src/views/layout/Footer/Footer.js
+++ client/src/views/layout/Footer/Footer.js
@@ -1,19 +1,36 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import 'assets/scss/layout/footer.scss';
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
   const userInfo = useSelector(state => state.user);
   console.log('Footer : ', userInfo.loginSuccess);
   const nowYear = () => {
     const _date = new Date();
     return _date.getFullYear();
   }
+
+  const userAction = () => {
+    if (userInfo.loginSuccess) {
+      // TODO Toast 컴포넌트 작업 후 토스트 컴포넌트 띄우기
+      axios.get('/api/users/logout')
+        .then(response => {
+          if (response.data.success) {
+            alert('로그아웃에 성공하였습니다.');
+          }
+        })
+    } else {
+      navigate('/login');
+    }
+
+  }
   return (
     <footer>
-      <p>Copyright &copy; <NavLink to={'/login'} className="btn-text-link">FreshPoSong</NavLink> {nowYear()}</p>
+      <p>Copyright &copy; <span className="btn-text-link" onClick={userAction}>FreshPoSong</span> {nowYear()}</p>
     </footer>
   )
 }
